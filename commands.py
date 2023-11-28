ae40e5798389e3c0ec1ea96621b03861ae5e9d93
Tim <timzgeybi01@gmail.com>
2023-11-28 19:19:38 +0300

    added feature of saving user id with their target language

--- commands.py
+++ commands.py
@@ -5,6 +5,7 @@ import API
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    dialogue.TO_LANGUAGE[update.effective_user.id] = 'en'
     await context.bot.send_message(chat_id=update.effective_chat.id, text="""Hey! Welcome to Yandex Translate bot!
 Here you can translate text from any language to a language of your choice!
 You don't need to specify source language, Yandex API will identify the language on its own :)
@@ -13,17 +14,24 @@ Use /help to get more help!""")
 
 
 async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(text=API.translate(dialogue.TO_LANGUAGE, update.message.text))
+    if update.effective_user.id not in dialogue.TO_LANGUAGE.keys():
+
+        await update.message.reply_text(text=
+                                        f"{API.translate(dialogue.TO_LANGUAGE[0], update.message.text)}")
+    else:
+        await update.message.reply_text(text=
+                                        f"{API.translate(dialogue.TO_LANGUAGE[update.effective_user.id], update.message.text)}")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text="""Available commands:
     1. /start - Restart the bot
-    2. /list_languages - List the languages to choose target languages
+    2. /choose_language - choose target language (the one you want to translate to)
     3. /help - Print this help message
     4. /cancel - To cancel choosing a language
-By default, I translate to english.
+No need to choose source language because I can identify the language on my own :)
+By default, I translate to English.
 To Translate text it's enough to send me a message with the text you need to translate.""")
 
 
--- dialogue.py
+++ dialogue.py
@@ -2,7 +2,7 @@ from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ContextTypes, ConversationHandler
 
 STATEONE: int = 0
-TO_LANGUAGE: str = 'ru'
+TO_LANGUAGE: dict = {0: 'en'}
 
 
 async def language_list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -29,33 +29,33 @@ async def first_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Fa
     print("Dialogue..first_answer")
     user_response = update.message.text
     if user_response == 'العربية':
-        await update.message.reply_text(text=f"I will start translating to العربية!")
-        TO_LANGUAGE = 'ar'
+        await update.message.reply_text(text=f"سأبدأ بالترجمة إلى اللغة العربية!")
+        TO_LANGUAGE[update.effective_user.id] = 'ar'
     elif user_response == 'English':
         await update.message.reply_text(text=f"I will start translating to English!")
-        TO_LANGUAGE = 'en'
+        TO_LANGUAGE[update.effective_user.id] = 'en'
     elif user_response == 'Русский':
-        await update.message.reply_text(text=f"I will start translating to Русский!")
-        TO_LANGUAGE = 'ru'
+        await update.message.reply_text(text=f"Начну переводить на русский!")
+        TO_LANGUAGE[update.effective_user.id] = 'ru'
     elif user_response == 'Español':
-        await update.message.reply_text(text=f"I will start translating to Español!")
-        TO_LANGUAGE = 'es'
+        await update.message.reply_text(text=f"¡Comenzaré a traducir al español!")
+        TO_LANGUAGE[update.effective_user.id] = 'es'
     elif user_response == 'Français':
-        await update.message.reply_text(text=f"I will start translating to Français!")
-        TO_LANGUAGE = 'fr'
+        await update.message.reply_text(text=f"Je vais commencer à traduire en français !")
+        TO_LANGUAGE[update.effective_user.id] = 'fr'
     elif user_response == 'हिन्दी':
-        await update.message.reply_text(text=f"I will start translating to हिन्दी!")
-        TO_LANGUAGE = 'hi'
+        await update.message.reply_text(text=f"मैं हिन्दी में अनुवाद करना शुरू करूँगा!")
+        TO_LANGUAGE[update.effective_user.id] = 'hi'
     elif user_response == 'فارسی':
-        await update.message.reply_text(text=f"I will start translating to فارسی!")
-        TO_LANGUAGE = 'fa'
+        await update.message.reply_text(text=f"من شروع به ترجمه به فارسی می کنم!")
+        TO_LANGUAGE[update.effective_user.id] = 'fa'
         return STATEONE
     elif user_response == 'Português':
-        await update.message.reply_text(text=f"I will start translating to Português!")
-        TO_LANGUAGE = 'pt'
+        await update.message.reply_text(text=f"Vou começar a traduzir para o Português!")
+        TO_LANGUAGE[update.effective_user.id] = 'pt'
     elif user_response == 'Deutsch':
-        await update.message.reply_text(text=f"I will start translating to Deutsch!")
-        TO_LANGUAGE = 'de'
+        await update.message.reply_text(text=f"Ik ga beginnen met vertalen naar het Duits!")
+        TO_LANGUAGE[update.effective_user.id] = 'de'
 
     return ConversationHandler.END
 
